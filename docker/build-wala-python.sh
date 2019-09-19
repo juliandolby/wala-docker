d71e46539c82248976be0c99453e57492e5b971e
Julian Dolby <juliandolby@gmail.com>
2019-09-19 13:51:31 -0400

    updates

--- docker/build-wala-python.sh
+++ docker/build-wala-python.sh
@@ -12,16 +12,23 @@ export LC_ALL=C.UTF-8
 
 cd /home/wala/git
 
-# use java 7 for jython
-echo 1 | sudo update-alternatives --config java
+# use java 7 for jython 2
+#echo 1 | sudo update-alternatives --config java
 
-# install jython
+# install jython 2
 hg clone https://bitbucket.org/juliandolby/jython
 cd jython
 ./gradlew build publishToMavenLocal
 
 # use java 8 again
-echo 0 | sudo update-alternatives --config java
+#echo 0 | sudo update-alternatives --config java
+
+# install jython 3
+cd /home/wala/git
+git clone https://github.com/jython/jython3
+cd jython3
+ant
+mvn install:install-file -Dfile=dist/jython-dev.jar -DgroupId=org.python -DartifactId=jython3 -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
 
 # install lsp4j with websockets
 cd /home/wala/git
@@ -35,8 +42,8 @@ cd /home/wala/git
 git clone https://github.com/wala/IDE
 
 # build LSP support
-cd IDE
-../WALA/gradlew build publishToMavenLocal
+cd IDE/com.ibm.wala.cast.lsp
+mvn clean install
 
 # clone WALA ML
 cd /home/wala/git
@@ -44,5 +51,6 @@ git clone https://github.com/wala/ML
 
 # build WALA
 cd ML
-../WALA/gradlew build publishToMavenLocal
+mvn clean install
+
 
